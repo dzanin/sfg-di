140cc8f7717cac2375e1975887587afdd2e10c6a
dzanin <davide.zanin@supsi.ch>
2021-02-21 13:59:40 +0100

    default profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
  * Created by dz on 21/02/21.
  */
 //If multiple service use application.properties to set active profile
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
